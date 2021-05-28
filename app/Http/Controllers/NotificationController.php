38165013078793942b38c8c6b3e406028173e9ac
Ristirianto Adi <ristiriantoadi@gmail.com>
2021-05-29 01:01:53 +0800

    move helper functions to notificationcontroller class

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -8,7 +8,7 @@ use App\Kendaraan;
 use App\Notifications\KartuExpired;
 use App\Notifications\SkExpired;
 use Illuminate\Support\Facades\View;
-use App\Helpers\Helper;
+// use App\Helpers\Helper;
 
 class NotificationController extends Controller
 {
@@ -67,5 +67,111 @@ class NotificationController extends Controller
         // return view('expired/kartu_expired',['kendaraans' => $kendaraans,'kartu_expired_notif_length' => $kendaraans_kartu_expired_notifications_length]);
     }
 
+
+    // helper functions
+    function update_status($expired_date){
+        $current_date = time();
+        $datediff = $expired_date - $current_date;
+        $datediff = round($datediff / (60 * 60 * 24));
+        
+        if($datediff<0){
+            return "expired";
+        }
+        else if($datediff<=30){
+            return "menjelang_expired";
+        }
+        else{
+            return "belum_expired";
+        }
+    }
+
+    function notification($old_status,$current_status,$type,$kendaraan){
+        // $user = Auth::user();
+        $users = User::all();
+        if($old_status == "belum_expired"){
+            if($current_status != "belum_expired"){
+                //add notifications
+                foreach ($users as $user) {
+                    if($type == "App\Notifications\SkExpired"){
+                        $user->notify(new SkExpired($kendaraan));
+                    }else{
+                        $user->notify(new KartuExpired($kendaraan));
+                    }
+                }
+            }
+        }
+        else{
+            if($current_status == "belum_expired"){
+                // remove_from_notification(record) / remove notifications / delete notifications
+                foreach ($users as $user) {
+                    foreach ($user->notifications as $notification) {
+                        if($notification->type == $type){
+                            $id_kendaraan =  $notification->data["kendaraan_id"];
+                            if ($kendaraan->id == $id_kendaraan){
+                                $user->notifications()->where('id', $notification->id)->get()->first()->delete();
+                                break;
+                            // return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    function check_status_sk($kendaraan){
+        if($kendaraan->tglakhirsk){
+            $old_status=$kendaraan->status_sk;
+            $kendaraan->status_sk=update_status(strtotime($kendaraan->tglakhirsk));
+            $kendaraan->save();
+
+            $current_status = $kendaraan->status_sk;
+            notification($old_status,$current_status,"App\Notifications\SkExpired",$kendaraan);
+        }
+    }
+    
+    function check_status_kartu($kendaraan){
+        if($kendaraan->masaberlaku){
+            $old_status = $kendaraan->status_kartu;
+            $kendaraan->status_kartu=update_status(strtotime($kendaraan->masaberlaku));
+            $kendaraan->save();
+    
+            $current_status = $kendaraan->status_kartu;
+            notification($old_status,$current_status,"App\Notifications\KartuExpired",$kendaraan);
+        }
+    }
+    
+    function get_notifications($type){
+        $user = Auth::user();
+        $kendaraans = [];
+        foreach ($user->notifications as $notification) {
+            if($notification->type == $type){
+                //get kendaraan
+                $id_kendaraan = $notification->data["kendaraan_id"];
+                $kendaraan = Kendaraan::find($id_kendaraan);
+                array_push($kendaraans,$kendaraan);
+            
+                $notification->markAsRead();
+            }
+            if($notification->data["kendaraan_id"] == 2051){
+                error_log("notification: ".$notification);
+            }
+        }
+        return $kendaraans;        
+    }
+    
+    function get_unread_notifications($type){
+        $user = Auth::user();
+        $kendaraans = [];
+        foreach ($user->unreadNotifications as $notification) {
+            if($notification->type == $type){
+                //get kendaraan
+                $id_kendaraan = $notification->data["kendaraan_id"];
+                $kendaraan = Kendaraan::find($id_kendaraan);
+                array_push($kendaraans,$kendaraan);
+            }
+        }
+        return $kendaraans;        
+    }
 
 }
