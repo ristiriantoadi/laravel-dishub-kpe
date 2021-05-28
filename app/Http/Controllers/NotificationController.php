df2e6986eab07c8ed37ec02adb39c85104ea26be
Ristirianto Adi <ristiriantoadi@gmail.com>
2021-05-29 01:08:26 +0800

    add User model

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Kendaraan;
+use App\User;
 use App\Notifications\KartuExpired;
 use App\Notifications\SkExpired;
 use Illuminate\Support\Facades\View;
