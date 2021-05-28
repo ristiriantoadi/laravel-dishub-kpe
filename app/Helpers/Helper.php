b30d3c539eeda1cf41a7fa50f86ffc441f5192e1
Ristirianto Adi <ristiriantoadi@gmail.com>
2021-05-28 11:14:55 +0800

    add notifications

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,59 +1,67 @@
 <?php
 
 // namespace App\Helpers;
+use App\Notifications\KartuExpired;
+use App\Notifications\SkExpired;
+use Illuminate\Support\Facades\Auth;
 
 if (!function_exists('update_status')) {
     function update_status($expired_date){
-        // $oldStatus=$kendaraan->status_sk;
-        
-        $current_date = time(); // or your date as well
-        // $expired_date = strtotime($kendaraan->tglakhirsk);
+        $current_date = time();
         $datediff = $expired_date - $current_date;
         $datediff = round($datediff / (60 * 60 * 24));
         
         if($datediff<0){
-            // $kendaraan->status_sk = "expired";
             return "expired";
         }
         else if($datediff<=30){
-            // $kendaraan->status_sk = "menjelang_expired";
             return "menjelang_expired";
         }
         else{
-            // $kendaraan->status_sk = "belum_expired";
             return "belum_expired";
         }
-        // $kendaraan->save();
+    }
+}
+
+if (!function_exists('notification')) {
+    function notification($old_status,$current_status,$type,$kendaraan){
+        if($old_status == "belum_expired"){
+            if($current_status != "belum_expired"){
+                //add notif
+                $user = Auth::user();
+                if($type == "status_sk"){
+                    $user->notify(new SkExpired($kendaraan));
+                }else{
+                    $user->notify(new KartuExpired($kendaraan));
+                }
+            }
+        }
+        else{
+            if($current_status == "belum_expired"){
+                // remove_from_notification(record)
+            }
+        }
     }
 }
 
 if (!function_exists('check_status_sk')) {
     function check_status_sk($kendaraan){
-        $oldStatus=$kendaraan->status_sk;
+        $old_status=$kendaraan->status_sk;
         $kendaraan->status_sk=update_status(strtotime($kendaraan->tglakhirsk));
         $kendaraan->save();
-        // $current_date = time(); // or your date as well
-        // $expired_date = strtotime($kendaraan->tglakhirsk);
-        // $datediff = $expired_date - $current_date;
-        // $datediff = round($datediff / (60 * 60 * 24));
-        
-        // if($datediff<0){
-        //     $kendaraan->status_sk = "expired";
-        // }
-        // else if($datediff<=30){
-        //     $kendaraan->status_sk = "menjelang_expired";
-        // }
-        // else{
-        //     $kendaraan->status_sk = "belum_expired";
-        // }
-        // $kendaraan->save();
+
+        $current_status = $kendaraan->status_sk;
+        notification($old_status,$current_status,"status_sk",$kendaraan);
     }
 }
  
 if (!function_exists('check_status_kartu')) {
     function check_status_kartu($kendaraan){
-        // error_log($kendaraan);
+        $old_status = $kendaraan->status_kartu;
         $kendaraan->status_kartu=update_status(strtotime($kendaraan->masaberlaku));
         $kendaraan->save();
+
+        $current_status = $kendaraan->status_sk;
+        notification($old_status,$current_status,"status_kartu",$kendaraan);
     }
 }
